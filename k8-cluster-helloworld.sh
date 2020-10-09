c8dd057f7e2401e7357b79efc1329b2113ce2a28
gregclinker <gregory.clinker@gmail.com>
2020-10-09 07:21:53 +0100

    added README

---
+++ k8-cluster-helloworld.sh
@@ -0,0 +1,19 @@
+#
+# example script to create a simple k8s cluster and deploy a hello world to it.
+# you will need to do a gcloud init before running this script
+#
+mvn -DskipTests com.google.cloud.tools:jib-maven-plugin:build -Dimage=gcr.io/kubernetes-demo-291914/hello-world:v1
+gcloud services enable compute.googleapis.com container.googleapis.com
+gcloud container clusters create gregs-cluster   --num-nodes 2   --machine-type n1-standard-1   --zone us-central1-c
+kubectl create deployment hello-world --image=gcr.io/kubernetes-demo-291914/hello-world:v1
+kubectl get deployments
+kubectl get pods
+kubectl create service loadbalancer hello-world --tcp=8080:8080
+kubectl get services
+kubectl get deployment
+kubectl get deployments
+kubectl scale deployment hello-world --replicas=3
+kubectl get deployments
+mvn package -DskipTests com.google.cloud.tools:jib-maven-plugin:build -Dimage=gcr.io/kubernetes-demo-291914/hello-world:v2
+kubectl set image deployment/hello-world hello-world=gcr.io/kubernetes-demo-291914/hello-world:v2
+kubectl get deployments
---
+++ src/main/java/com/essexboy/helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package com.essexboy.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "hello Greg K8s demo!";
+	}
+
+}
